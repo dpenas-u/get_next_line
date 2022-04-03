c9c2dcdae74c398d0f421e3b870de25649a2b2ab
Dani <dani@iMac-de-Dani.local>
2022-04-03 10:20:46 -0700

    PRUEBAS 2 casa

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/uio.h>
@@ -7,16 +8,22 @@
 int	main(int argc, char *argv[])
 {
 	int	fd;
-	char	buffer[BUFFER_SIZE + 1];
+	char	buf;
 	int	ret;
+	char	*buffer;
 
 	if (argc > 1)
 	{
 		fd = open(argv[1], O_RDONLY);
+		while(read(fd, &buf, 1) && buf != 10)
+			printf("%c", buf);
+		printf("\n");
+		buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		ret = read(fd, buffer, BUFFER_SIZE);
 		printf("ret: %d\n", ret);
 		buffer[ret] = 0;
 		printf("buffer: %s\n", buffer);
 		close(fd);
 	}
+	system("leaks a.out");
 }
